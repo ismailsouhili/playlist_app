c094b8b44c1db4a0a2f66f6d064a2d727f6eb6bf
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-02-10 09:15:37 +0000

    edit config

--- config/database.php
+++ config/database.php
@@ -57,13 +57,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                 //PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                 //PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'),
-                 //PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'),
-                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/app/.ssl/cert.pem'), 
-                 ]) : [],
             
            
         ],
