bcd444bb837506ac6ceaab86b29e9a13de7a410c
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-02-10 07:58:20 +0000

    edit config DB

--- config/database.php
+++ config/database.php
@@ -60,8 +60,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/cert.pem',
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'),
+                ]) : [],
         ],
 
         'mariadb' => [
