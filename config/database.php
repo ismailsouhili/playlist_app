eb98f7e8c2c76a48977286d4f4b1b9399efefc4c
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-02-10 08:18:07 +0000

    edit config

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL','mysql://root:<PASSWORD>@8e8zju.stackhero-network.com:6844/root?useSSL=true&requireSSL=true'),
+            'url' => env('DB_URL','mysql://root:<PASSWORD>@8e8zju.stackhero-network.com:6844/root?useSSL=false&requireSSL=false'),
             'host' => env('DB_HOST', '8e8zju.stackhero-network.com'),
             'port' => env('DB_PORT', '6844'),
-            'database' => env('DB_DATABASE', 'playlist'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'OfrIyc8COCug7J1qDHeiwXCHU48CzBL3'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -57,6 +57,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/cert.pem'),
+                 ]) : [],
            
         ],
 
