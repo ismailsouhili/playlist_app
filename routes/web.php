ba102960114349eea55f7d3f11a3465592eb5b36
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-02-13 20:31:56 +0000

    edit pl-cont

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,10 @@ Route::post('/playlists', [PlaylistController::class, 'store'])->name('playlists
 
 // Route für Import und Export
 Route::get('export-csv', [PlaylistController::class, 'exportCSV'])->name('exportCSV');
-Route::post('/playlists/import', action: [PlaylistController::class, 'importCSV'])->name('playlists.import');
+//Route::post('/playlists/import', action: [PlaylistController::class, 'importCSV'])->name('playlists.import');
+
+Route::post('/playlists/import', [PlaylistController::class, 'importCSV'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class])->name('playlists.import');
+
 
 
 
