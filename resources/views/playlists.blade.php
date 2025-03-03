b4c8d3bf85a1df822e81102ac09ce524cfb125d4
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-03-03 17:43:22 +0100

    Update playlists.blade.php

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -678,6 +678,12 @@
 <script src="/js/demo/chart-area-demo.js"></script>
 <script src="/js/demo/chart-pie-demo.js"></script>
 
+
+    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+    <script src="{{ asset('js/demo/chart-bar-demo.js') }}"></script>
+
+
    
 
 
