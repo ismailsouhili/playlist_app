511447c01223cd2048959a11b26fbddbfd078e09
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-03-03 17:33:50 +0100

    Update playlists.blade.php

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -678,6 +678,9 @@
 <script src="/js/demo/chart-area-demo.js"></script>
 <script src="/js/demo/chart-pie-demo.js"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
 
 
 
