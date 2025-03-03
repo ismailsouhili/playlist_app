7cf080071ce79b9986b4d5baec9755365ac5af59
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2025-03-03 18:01:40 +0100

    Update playlists.blade.php

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <!-- <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">-->
+     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
@@ -679,9 +679,25 @@
 <script src="/js/demo/chart-pie-demo.js"></script>
 
 
-    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+<!-- jQuery -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap JS -->
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Core plugin JavaScript -->
+<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for all pages -->
+<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
 <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
-    <script src="{{ asset('js/demo/chart-bar-demo.js') }}"></script>
+<script src="{{ asset('js/demo/chart-bar-demo.js') }}"></script>
 
 
    
